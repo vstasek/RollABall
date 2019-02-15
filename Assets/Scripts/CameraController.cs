0b07befed028a906bbae52c6f8f31e1d25584d1c
Victor Stasek <stasekvictor@gmail.com>
2019-02-15 12:57:25 -0500

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this to attach camera to player object
+	public GameObject player;
+
+	// make this private because we can set it in the script
+	private Vector3 offset;
+
+	// initialization
+	void Start () {
+		// subtract transform of player from transform of camera to find difference between the two
+		offset = transform.position - player.transform.position;
+	}
+	
+	// for following cameras, procedural animation, and gathering last known states, use LateUpdate().
+	// LateUpdate runs every frame like Update but is garunteed to run after all items have been processed in Update.
+	// Using this, we know that the player has already moved for this frame.
+	void LateUpdate () {
+		// as we move the player object the camera's position is updated based on the player's position.
+		// This is processed before displaying what the camera can "see"
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿//Check every frame for input and apply that input to the Player game object as movement
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+// Ctrl + '
+
+//Update() is called before rendering a frame
+//FixedUpdate() is called before performing any physics calculations
+
+
+public class PlayerController : MonoBehaviour 
+{
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	/*Rigidbody objects allow control of an object's position through physics simulation.
+	 * 
+	Adding a Rigidbody component to an object will put its motion under the control of 
+	Unity's physics engine. Even without adding any code, a Rigidbody object will be pulled 
+	downward by gravity and will react to collisions with incoming objects if the right 
+	Collider component is also present. */
+	private Rigidbody rb;
+
+	private int count;
+
+	// Start() gets called on the first frame that the script is active (usually first frame of game)
+	void Start ()
+	{
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+
+		// get access to actual RigidBody component of our Player object
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+	{
+		// Stores player input read from the keyboard
+		// Automatically uses arrow keys. How do I use different keys?...
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// Create a vector out of the player input data to apply to RigidBody
+			// y is 0 because we don't want to move up
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	// This will be called when Player first touches a trigger collider.
+	// 'other' is a reference to the Collider we have touched
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			count++;
+			SetCountText ();
+			other.gameObject.SetActive (false);
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+
+		if (count >= 16) 
+		{
+			winText.text = "CONGRATULATIONS!\nYOU WIN FOREVER!";
+		}
+	}
+}
